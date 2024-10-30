a55970d98bc4c58e7f1e83377b25ae06f4ffe06a
Tiago Andre Batista Rodrigues <79528246+TiagoRodrigues-11@users.noreply.github.com>
2024-10-30 14:46:53 +0000

    Time: 16 ms (89.57%) | Memory: 59.3 MB (99.95%) - LeetX Squads

---
+++ Easy/0217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for(int num: nums) {
+            set.add(num);
+        }
+
+        return set.size() != nums.length;
+
+    }
+}
