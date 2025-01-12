33b68dd71ffdeb88d0e5847067ea0367a4da1d2a
Tiago Andre Batista Rodrigues <79528246+TiagoRodrigues-11@users.noreply.github.com>
2025-01-12 18:47:01 +0000

    Time: 0 ms (100.00%) | Memory: 40.6 MB (99.98%) - LeetX Squads

---
+++ Easy/0202-happy-number/happy-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if(n == 1 || n == 7 ) return true;
+        else if(n < 10) return false;
+        else {
+            int sum = 0;
+
+            while (n > 0) {
+                int value = n % 10;
+                sum += Math.pow(value, 2);
+                n /= 10;    
+            } 
+
+            return isHappy(sum);
+        }
+
+    }
+}
