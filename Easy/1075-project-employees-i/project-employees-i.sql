863c90440af781ad79e63336401dba402d43fdef
Tiago Andre Batista Rodrigues <79528246+TiagoRodrigues-11@users.noreply.github.com>
2025-05-05 15:04:23 +0000

    Time: 591 ms (10.38%) | Memory: 0B (100.00%) - LeetX Squads

---
+++ Easy/1075-project-employees-i/project-employees-i.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+
+SELECT 
+    p.project_id,
+    ROUND (SUM(experience_years) * 1.0 / COUNT(p.employee_id), 2) AS average_years
+FROM Project p
+JOIN Employee e ON p.employee_id = e.employee_id
+GROUP BY p.project_id
